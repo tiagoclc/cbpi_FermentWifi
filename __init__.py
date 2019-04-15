b16fe591c59bef4809fc4a03048ff1d01036ca4a
tiagoclc <tiagoclc@hotmail.com>
2019-04-15 15:18:38 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,105 +1,177 @@
 # -*- coding: utf-8 -*-
-import os
-from subprocess import Popen, PIPE, call
-
-from modules import cbpi
-from modules.core.hardware import ActorBase, SensorPassive, SensorActive
-from modules.core.props import Property
-import json
-import httplib2
-import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
-import time
-from flask import Blueprint, render_template, jsonify, request
-
+from eventlet import Queue
+from modules import cbpi, app, ActorBase
+from modules.core.hardware import SensorActive
+import json
+import os, re, threading, time
+from modules.core.props import Property
 
 
 
 blueprint = Blueprint('FermentWifi', __name__)
-cache = {}
-client = mqtt.Client()
-
-Broker = "127.0.0.1"
-PortaBroker = 5000
-KeepAliveBroker = 60
-TimeoutConexao = 5 #em segundos
 
 
+q = Queue()
 
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(Broker, PortaBroker, KeepAliveBroker)
- 
- 
-ThMQTT = Thread(target=client.loop_forever)
+def on_connect(client, userdata, flags, rc):
+    print("MQTT Connected" + str(rc))
 
+class MQTTThread (threading.Thread):
 
+    def __init__(self,server,port,username,password,tls):
+        threading.Thread.__init__(self)
+        self.server = server
+        self.port = port
+        self.username = username
+        self.password = password
+        self.tls = tls
+
+    client = None
+    def run(self):
+        self.client = mqtt.Client()
+        self.client.on_connect = on_connect
+
+        #if self.username != "username" and self.password != "password":
+        #    self.client.username_pw_set(self.username, self.password)
+        
+        if self.tls.lower() == 'true':
+            self.client.tls_set_context(context=None)
+
+        self.client.connect(str(self.server), int(self.port), 60)
+        self.client.loop_forever()
 
 @cbpi.actor
 class FermentWifiActor(ActorBase):
 
-	key1 = Property.Text(label="Nome do FermentWifi (ex: FW_0000)", configurable=True)
+
 
 	usar = Property.Select("O que utilizar do FermentWifi", options=["Aquecedor", "Resfriador"], description="Escolher o que usar do FermentWifi")
 
-	def send(self, command):
-        	try:
-                	h = httplib2.Http(".cache")
-	                (resp, content) = h.request("%s/%s" % ("http://fermentwifi.local:80", command), "GET", headers={'cache-control':'no-cache'})
-        	except Exception as e:
-                	self.api.app.logger.error("Falha ao tentar controlar o ator do FermentWifi: %s/%s" % ("http://fermentwifi.local:80", command))
+    topic = Property.Text(label="Nome do FermentWifi (ex: FW_0000)", configurable=True)
+	
+	topic=topic+"_Raspi"
 
-	TopicoPublish = key1 + "_Raspi"
-	global client
-
-
-	ThMQTT.start()
-
-
-
-	def on(self, power=None):
-		if self.usar=="Resfriador":
-			client.publish(TopicoPublish,"0")
+    def on(self, power=100):
+        
+   		if self.usar=="Resfriador":
+			#self.send("C?p=14&e=1")
 			#self.send("ControleCraftLiga?pino=14&estado=1")
+			
+			self.api.cache["mqtt"].client.publish(self.topic, payload="0", qos=2, retain=True)
+			
 		elif self.usar=="Aquecedor":
-			client.publish(TopicoPublish,"1")
-
-	def off(self):
-		if self.usar=="Resfriador":
-			client.publish(TopicoPublish,"2")
+			#self.send("C?p=12&e=1")
+			self.api.cache["mqtt"].client.publish(self.topic,payload="1", qos=2, retain=True)
+        
+    def off(self):
+   		if self.usar=="Resfriador":
+			#self.send("C?p=14&e=1")
+			#self.send("ControleCraftLiga?pino=14&estado=1")
+			
+			self.api.cache["mqtt"].client.publish(self.topic, payload="2", qos=2, retain=True)
+			
 		elif self.usar=="Aquecedor":
-			client.publish(TopicoPublish,"3")
+			#self.send("C?p=12&e=1")
+			self.api.cache["mqtt"].client.publish(self.topic,payload="3", qos=2, retain=True)
+			
 
 @cbpi.sensor
 class FermentWifiSensor(SensorActive):
-	key = Property.Text(label="Nome do FermentWifi (ex: FW_0000)", configurable=True)
+    a_topic = Property.Text("Topic", configurable=True, default_value="", description="MQTT TOPIC")
+    b_payload = Property.Text("Payload Dictioanry", configurable=True, default_value="", description="Where to find msg in patload, leave blank for raw payload")
+    c_unit = Property.Text("Unit", configurable=True, default_value="", description="Units to display")
 
-	TopicoPublish = key + "_Raspi"
+    last_value = None
+    def init(self):
+        self.topic = self.a_topic
+        if self.b_payload == "":
+            self.payload_text = None
+        else:
+            self.payload_text = self.b_payload.split('.')
+        self.unit = self.c_unit[0:3]
 
-	def execute(self):
-		global cache
-		while self.is_running():
-			try:
-				value = cache.pop(self.key, None)
-				if value is not None:
-					self.data_received(value)
-			except:
-				pass
+        SensorActive.init(self)
+        def on_message(client, userdata, msg):
+            
+            try:
+                print "payload " + msg.payload        
+                json_data = json.loads(msg.payload)
+                #print json_data
+                val = json_data
+                if self.payload_text is not None:
+                    for key in self.payload_text:
+                        val = val.get(key, None)
+                #print val
+                if isinstance(val, (int, float, basestring)):
+                    q.put({"id": on_message.sensorid, "value": val})
+            except Exception as e:
+                print e
+        on_message.sensorid = self.id
+        self.api.cache["mqtt"].client.subscribe(self.topic)
+        self.api.cache["mqtt"].client.message_callback_add(self.topic, on_message)
+
+
+    def get_value(self):
+        return {"value": self.last_value, "unit": self.unit}
+
+    def get_unit(self):
+        return self.unit
+
+    def stop(self):
+        self.api.cache["mqtt"].client.unsubscribe(self.topic)
+        SensorActive.stop(self)
+
+    def execute(self):
+        '''
+        Active sensor has to handle his own loop
+        :return: 
+        '''
+        self.sleep(5)
+
+@cbpi.initalizer(order=0)
+def initMQTT(app):
+
+    server = app.get_config_parameter("MQTT_SERVER",None)
+    if server is None:
+        server = "127.0.0.1"
+        cbpi.add_config_parameter("MQTT_SERVER", "localhost", "text", "MQTT Server")
+
+    port = app.get_config_parameter("MQTT_PORT", None)
+    if port is None:
+        port = "1883"
+        cbpi.add_config_parameter("MQTT_PORT", "1883", "text", "MQTT Sever Port")
+
+    username = app.get_config_parameter("MQTT_USERNAME", None)
+    if username is None:
+        username = ""
+        cbpi.add_config_parameter("MQTT_USERNAME", "username", "text", "MQTT username")
+
+    password = app.get_config_parameter("MQTT_PASSWORD", None)
+    if password is None:
+        password = ""
+        cbpi.add_config_parameter("MQTT_PASSWORD", "password", "text", "MQTT password")
+
+    tls = app.get_config_parameter("MQTT_TLS", None)
+    if tls is None:
+        tls = "false"
+        cbpi.add_config_parameter("MQTT_TLS", "false", "text", "MQTT TLS")
+
+    app.cache["mqtt"] = MQTTThread(server,port)
+    app.cache["mqtt"].daemon = True
+    app.cache["mqtt"].start()
+    
+    def mqtt_reader(api):
+        while True:
+            try:
+                m = q.get(timeout=0.1)
+                api.cache.get("sensors")[m.get("id")].instance.last_value = m.get("value")
+                api.receive_sensor_value(m.get("id"), m.get("value"))
+            except:
+                pass
+
+cbpi.app.register_blueprint(blueprint, url_prefix='/api/fermentwifi')
 	
-			self.api.socketio.sleep(1)
+os.system("sudo mv ~/craftbeerpi3/modules/plugins/cbpi_FermentWifi/esp.service /etc/avahi/services/ | sudo avahi-daemon -r")
 
-
-def on_message(client, userdata, msg):
-    cache = str(msg.payload)
-        print("[MSG RECEBIDA] Topico: "+msg.topic+" / Mensagem: "+MensagemRecebida)
-    return
-
-	
-
-@cbpi.initalizer()
-def init(cbpi):
-	print "INICIALIZA O MODULO FERMENTWIFI"
-	cbpi.app.register_blueprint(blueprint, url_prefix='/api/fermentwifi')
-	print "READY"
-	os.system("sudo mv ~/craftbeerpi3/modules/plugins/cbpi_FermentWifi/esp.service /etc/avahi/services/ | sudo avahi-daemon -r")
-
+cbpi.socketio.start_background_task(target=mqtt_reader, api=app)
