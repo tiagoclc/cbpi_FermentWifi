5ea425faf01f7a523a5382148400449a1416d489
tiagoclc <tiagoclc@hotmail.com>
2019-04-15 14:26:24 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,6 +7,9 @@ from modules.core.hardware import ActorBase, SensorPassive, SensorActive
 from modules.core.props import Property
 import json
 import httplib2
+import paho.mqtt.publish as publish
+import paho.mqtt.client as mqtt
+import time
 from flask import Blueprint, render_template, jsonify, request
 
 
@@ -14,12 +17,28 @@ from flask import Blueprint, render_template, jsonify, request
 
 blueprint = Blueprint('FermentWifi', __name__)
 cache = {}
+client = mqtt.Client()
 
+Broker = "127.0.0.1"
+PortaBroker = 5000
+KeepAliveBroker = 60
+TimeoutConexao = 5 #em segundos
+
+
+
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(Broker, PortaBroker, KeepAliveBroker)
+ 
+ 
+ThMQTT = Thread(target=client.loop_forever)
 
 
 
 @cbpi.actor
 class FermentWifiActor(ActorBase):
+
+	key1 = Property.Text(label="Nome do FermentWifi (ex: FW_0000)", configurable=True)
 
 	usar = Property.Select("O que utilizar do FermentWifi", options=["Aquecedor", "Resfriador"], description="Escolher o que usar do FermentWifi")
 
@@ -30,21 +49,33 @@ class FermentWifiActor(ActorBase):
         	except Exception as e:
                 	self.api.app.logger.error("Falha ao tentar controlar o ator do FermentWifi: %s/%s" % ("http://fermentwifi.local:80", command))
 
+	TopicoPublish = key1 + "_Raspi"
+	global client
+
+
+	ThMQTT.start()
+
+
+
 	def on(self, power=None):
 		if self.usar=="Resfriador":
-			self.send("C?p=14&e=1")
+			client.publish(TopicoPublish,"0")
+			#self.send("ControleCraftLiga?pino=14&estado=1")
 		elif self.usar=="Aquecedor":
-			self.send("C?p=12&e=1")
+			client.publish(TopicoPublish,"1")
 
 	def off(self):
 		if self.usar=="Resfriador":
-			self.send("Co?p=14&e=0")
+			client.publish(TopicoPublish,"2")
 		elif self.usar=="Aquecedor":
-			self.send("Co?p=12&e=0")
+			client.publish(TopicoPublish,"3")
 
 @cbpi.sensor
 class FermentWifiSensor(SensorActive):
 	key = Property.Text(label="Nome do FermentWifi (ex: FW_0000)", configurable=True)
+
+	TopicoPublish = key + "_Raspi"
+
 	def execute(self):
 		global cache
 		while self.is_running():
