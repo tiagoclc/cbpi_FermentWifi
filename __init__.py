8973a3f4cf7ebc14ee863814e77482c460955a4e
tiagoclc <tiagoclc@hotmail.com>
2019-04-19 22:12:53 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -125,7 +125,8 @@ def initMQTT(app):
 	file = open("/home/pi/craftbeerpi3/modules/plugins/FermentWifiPlugin/roda.txt","r")
 	if file.read()=="sim":
 		file.close()  
-		os.system("sudo mv /home/pi/craftbeerpi3/modules/plugins/FermentWifiPlugin/esp.service /etc/avahi/services/ | sudo avahi-daemon -r | sudo apt-get install mosquitto mosquitto-clients -y | sudo pip install -q paho-mqtt | sudo service mosquitto stop | sudo mv /home/pi/craftbeerpi3/modules/plugins/FermentWifiPlugin/mosquitto.conf /etc/mosquitto/ | sudo systemctl enable mosquitto | sudo service mosquitto start")
+		os.system("sudo su")
+		os.system("mv /home/pi/craftbeerpi3/modules/plugins/FermentWifiPlugin/esp.service /etc/avahi/services/ | avahi-daemon -r | apt-get install mosquitto mosquitto-clients -y | pip install -q paho-mqtt | mv /home/pi/craftbeerpi3/modules/plugins/FermentWifiPlugin/mosquitto.conf /etc/mosquitto/ | systemctl enable mosquitto | sudo service mosquitto restart")
 		file = open("/home/pi/craftbeerpi3/modules/plugins/FermentWifiPlugin/roda.txt","w")
 		file.write("nao")
 		file.close()
