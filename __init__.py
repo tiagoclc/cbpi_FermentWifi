3b4c4965c29f4903d3420dee5ded7bfe06ec81f4
tiagoclc <tiagoclc@hotmail.com>
2019-05-03 22:20:13 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -187,24 +187,24 @@ def update_compressors(api):
 	for Resfriador_FermentWifi in cbpi.gpio_compressors:
         	if Resfriador_FermentWifi.delayed and datetime.utcnow() >= Resfriador_FermentWifi.compressor_wait:
 			if Resfriador_FermentWifi.compressor_on==False:
-				#Resfriador_FermentWifi.topic=Resfriador_FermentWifi.key0+"_RaspiOnOff"
-				#mqttc.publish(Resfriador_FermentWifi.topic,"0")
+				Resfriador_FermentWifi.topic=Resfriador_FermentWifi.key0+"_RaspiOnOff"
+				mqttc.publish(Resfriador_FermentWifi.topic,"0")
 				print("enviado liga resfriador")
 				print(Resfriador_FermentWifi.topic)
 				cbpi.app.logger.info("enviado liga resfriador após atraso")
 				Resfriador_FermentWifi.compressor_on=True
-				Resfriador_FermentWifi.on()
+				#Resfriador_FermentWifi.on()
 
 	for Aquecedor_FermentWifi in cbpi.gpio_compressors2:
 		if Aquecedor_FermentWifi.delayed2 and datetime.utcnow() >= Aquecedor_FermentWifi.compressor_wait2:
 			if Aquecedor_FermentWifi.compressor_on2==False:
-				#Aquecedor_FermentWifi.topic=Aquecedor_FermentWifi.key0+"_RaspiOnOff"
-				#mqttc.publish(Aquecedor_FermentWifi.topic,"1")
+				Aquecedor_FermentWifi.topic=Aquecedor_FermentWifi.key0+"_RaspiOnOff"
+				mqttc.publish(Aquecedor_FermentWifi.topic,"1")
 				print("enviado liga aquecedor")
 				print(Aquecedor_FermentWifi.topic)
 				cbpi.app.logger.info("enviado liga aquecedor após atraso")
 				Aquecedor_FermentWifi.compressor_on2=True
-				Aquecedor_FermentWifi.on()
+				#Aquecedor_FermentWifi.on()
 
 				
 				
